36e54835ec74b5285165e22a8d1a4fefe529fadb
Gabrielegl <gabriel.leao@relyon.dev.br>
2023-05-28 17:17:01 -0300

    tweak the name of some user variables

--- lib/dependency_injection/share/injector.dart
+++ lib/dependency_injection/share/injector.dart
@@ -1,6 +1,6 @@
 //import 'package:pokemon/dependency_injection/features/pokemon/pokemon_injector.dart';
 
-import 'package:news_list/dependency_injection/features/pokemon/pokemon_injector.dart';
+import 'package:news_list/dependency_injection/features/user/user_injector.dart';
 
 class Injector {
   call() {
